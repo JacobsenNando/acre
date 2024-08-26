4e25db0390a0e7bcb6cdeaf2adf84f15ede7d3b4
JacobsenNando <fernandofjlr@gmail.com>
2024-08-26 13:43:08 -0300

    Update server_mail

--- src/lib/server/mail.server.ts
+++ src/lib/server/mail.server.ts
@@ -1,15 +1,14 @@
-import nodemailer from 'nodemailer';
-import { SENDER_EMAIL, SENDER_PASSWORD } from '$env/static/private';
+import nodemailer from "nodemailer";
+import { SENDER_EMAIL, SENDER_PASSWORD } from "$env/static/private";
 
 const transporter = nodemailer.createTransport({
-	name: 'Site Hanrry!',
-	host: 'smtp.gmail.com',
-	port: 587,
-	secure: false,
-	auth: {
-		user: `${SENDER_EMAIL}`,
-		pass: `${SENDER_PASSWORD}`
-	}
+  host: "smtp.gmail.com",
+  port: 465, // Porta para STARTTLS
+  secure: true, // Use false para STARTTLS
+  auth: {
+    user: SENDER_EMAIL,
+    pass: SENDER_PASSWORD,
+  },
 });
 
 export default transporter;
