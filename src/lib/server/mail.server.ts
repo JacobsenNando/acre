ecab081fc1ed3cd7237917ac53950da57e5e7cab
JacobsenNando <fernandofjlr@gmail.com>
2024-08-26 11:01:34 -0300

    up

--- src/lib/server/mail.server.ts
+++ src/lib/server/mail.server.ts
@@ -1,14 +1,13 @@
 import nodemailer from 'nodemailer';
-import { SENDER_EMAIL, SENDER_PASSWORD } from '$env/static/private';
+// import { SENDER_EMAIL, SENDER_PASSWORD } from '$env/static/private';
 
 const transporter = nodemailer.createTransport({
-	name: 'Site Hanrry!',
 	host: 'smtp.gmail.com',
 	port: 587,
 	secure: true,
 	auth: {
-		user: `${SENDER_EMAIL}`,
-		pass: `${SENDER_PASSWORD}`
+		user: `sitehanrry@gmail.com`,
+		pass: `daib vups rpvq jfhx`
 	}
 });
 
