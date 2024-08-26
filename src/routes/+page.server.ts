d8d2ad78fafa57dbc0fc86849f24fa76aeddae41
JacobsenNando <fernandofjlr@gmail.com>
2024-08-26 11:35:25 -0300

    Novo commit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,38 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { Actions } from './$types';
-import transporter from '$lib/server/mail.server';
-import { SENDER_EMAIL, EMAIL_RECEVER } from '$env/static/private';
-import sanitizeHtml from 'sanitize-html';
-
+import type { Actions } from "./$types";
+import transporter from "$lib/server/mail.server";
+import { SENDER_EMAIL, EMAIL_RECEVER } from "$env/static/private";
+import sanitizeHtml from "sanitize-html";
+//commit
 export const actions: Actions = {
-	enviarEmail: async ({ request }) => {
-		try {
-			const formData = await request.formData();
+  enviarEmail: async ({ request }) => {
+    try {
+      const formData = await request.formData();
 
-			const nome: any = formData.get('nome');
-			const telefone: any = formData.get('telefone');
-			const mensagem: any = formData.get('mensagem');
+      const nome: any = formData.get("nome");
+      const telefone: any = formData.get("telefone");
+      const mensagem: any = formData.get("mensagem");
 
-			console.log(nome);
-			console.log(telefone);
-			console.log(mensagem);
+      console.log(nome);
+      console.log(telefone);
+      console.log(mensagem);
 
-			const nomeSanitized = sanitizeHtml(nome, {
-				allowedTags: sanitizeHtml.defaults.allowedTags,
-				allowedAttributes: {}
-			});
+      const nomeSanitized = sanitizeHtml(nome, {
+        allowedTags: sanitizeHtml.defaults.allowedTags,
+        allowedAttributes: {},
+      });
 
-			const telefoneSanitized = sanitizeHtml(telefone, {
-				allowedTags: sanitizeHtml.defaults.allowedTags,
-				allowedAttributes: {}
-			});
+      const telefoneSanitized = sanitizeHtml(telefone, {
+        allowedTags: sanitizeHtml.defaults.allowedTags,
+        allowedAttributes: {},
+      });
 
-			const sanitizedmensagem = sanitizeHtml(mensagem, {
-				allowedTags: sanitizeHtml.defaults.allowedTags,
-				allowedAttributes: {}
-			});
+      const sanitizedmensagem = sanitizeHtml(mensagem, {
+        allowedTags: sanitizeHtml.defaults.allowedTags,
+        allowedAttributes: {},
+      });
 
-			const html = `
+      const html = `
 			   <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; text-align: center;">
 			     <div style="background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 4px 8px rgba(0,0,0,0.1); text-align: left;">
 			       <h1 style="color: #333; text-align: center;">Contato via site</h1>
@@ -44,30 +44,30 @@ export const actions: Actions = {
 			   </div>
 			 `;
 
-			const send = async () => {
-				const info = await transporter.sendMail({
-					from: `${SENDER_EMAIL}`,
-					to: `${EMAIL_RECEVER}`,
-					subject: `Site Hanrry - Contato de ${nomeSanitized}`,
-					html: html
-				});
-				console.log('Mensagem Enviada:', info);
-			};
+      const send = async () => {
+        const info = await transporter.sendMail({
+          from: `${SENDER_EMAIL}`,
+          to: `${EMAIL_RECEVER}`,
+          subject: `Site Hanrry - Contato de ${nomeSanitized}`,
+          html: html,
+        });
+        console.log("Mensagem Enviada:", info);
+      };
 
-			await send();
+      await send();
 
-			return {
-				status: 200,
-				message: 'Email enviado com sucesso'
-			};
-		} catch (error) {
-			console.error(error);
-			return {
-				status: 500,
-				body: {
-					message: 'Erro ao enviar email'
-				}
-			};
-		}
-	}
+      return {
+        status: 200,
+        message: "Email enviado com sucesso",
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        status: 500,
+        body: {
+          message: "Erro ao enviar email",
+        },
+      };
+    }
+  },
 };
